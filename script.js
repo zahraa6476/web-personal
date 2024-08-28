dcd3ef332a4ef53e758df16aba8fe93f61aedbb3
zahraa6476 <zahranurazizia@gmail.com>
2024-08-28 11:34:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// text animation
+var typed = new Typed(".typing", {
+            strings: ["Only One"],
+            typeSpeed: 200, 
+            backSpeed: 150,
+            cursorChar: ' _ ',
+            smartBackspace: true,
+            loop: true,
+        });
