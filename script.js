684f6d3ff47db84e54c7b6d9238c021963f080e9
zahraa6476 <zahranurazizia@gmail.com>
2024-08-28 11:38:58 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// text animation
-var typed = new Typed(".typing", {
-            strings: ["Only One"],
-            typeSpeed: 200, 
-            backSpeed: 150,
-            cursorChar: ' _ ',
-            smartBackspace: true,
-            loop: true,
-        });
